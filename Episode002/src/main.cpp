f8b979699f5f87a034248a4b45e3ee1694eea12a
Seonguk Choi <chsu7086@gmail.com>
2023-08-18 06:51:19 +0900

    Episode 002 Code

---
+++ Episode002/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+template <typename T>
+void add(T x, T y) {
+    std::cout << x+y << std::endl;
+}
+
+int main() {
+    int a = 3;
+    int b = 7;
+
+    std::cout << "Hello from C++ 23 with CMake on Windows/Linux." << std::endl;
+
+    add(a, b);
+
+    return 0;
+}
