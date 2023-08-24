98ed036555349fdfb51edb1905c9b7aed135e743
Seonguk Choi <chsu7086@gmail.com>
2023-08-25 02:03:48 +0900

    Some minor changes in source files (Windows 11)

--- Episode005/source/src/timer.cpp
+++ Episode005/source/src/timer.cpp
@@ -5,11 +5,11 @@ Timer::Timer(int s) {
 }
 
 void Timer::get_start() {
-    std::cout << "The timer starts count-down" << start << "seconds before the event." << std::endl;
+    std::cout << std::endl << "The timer starts count-down " << start << " seconds before the event." << std::endl;
 }
 
 void Timer::set_start() {
-    std::cout << "How many seconds before the timer starts count-down? ";
+    std::cout << std::endl << "How many seconds before the timer starts count-down? ";
     std::cin >> start;
 }
 
@@ -25,7 +25,7 @@ void Timer::turn_on() {
     int ans = 0;
 
     while(true) {
-        std::cout << "Would you start the countdown? ";
+        std::cout << std::endl << "Would you start the countdown? ";
         std::cout << "(0: proceed to the countdown / 1: check the starting time / 2: set the time) ";
         std::cin >> ans;
 
@@ -37,7 +37,7 @@ void Timer::turn_on() {
         } else if (ans == 2) {
             set_start();
         } else {
-            std::cout << "It's invalid input. Try again." << std::endl;
+            std::cout << std::endl << "It's invalid input. Try again." << std::endl;
         }
     }
 }
